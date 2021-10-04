ae83a01d9ad1f05ce94af68a72d451af212c508e
xo <revolutionaparis@gmail.com>
2021-10-04 23:31:15 +0200

    Display text game won false on creation

---
+++ app/src/androidTest/java/com/xo/iksoks/GameActivityTest.kt
@@ -0,0 +1,28 @@
+package com.xo.iksoks
+
+import androidx.compose.ui.test.junit4.createAndroidComposeRule
+import androidx.compose.ui.test.onNodeWithText
+import org.junit.Rule
+import org.junit.Test
+
+class GameActivityTest{
+
+    @get:Rule
+    val composeAndroidTestRule = createAndroidComposeRule<GameActivity>()
+
+
+    @Test
+    fun shouldDisplayTextGameWonFalseOnCreation(){
+        composeAndroidTestRule
+            .onNodeWithText("Game won: false")
+            .assertExists()
+    }
+
+    @Test
+    fun shouldAssertTextGameWonTrueDoesNotExist(){
+        composeAndroidTestRule
+            .onNodeWithText("Game won: true")
+            .assertDoesNotExist()
+    }
+
+}
--- app/src/main/java/com/xo/iksoks/GameActivity.kt
+++ app/src/main/java/com/xo/iksoks/GameActivity.kt
@@ -3,14 +3,28 @@ package com.xo.iksoks
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import com.xo.iksoks.ui.theme.IksOksTheme
 
 class GameActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        val game = IksOks().apply { setup() }
+
         setContent {
             IksOksTheme {
-                //Todo Setup UI
+                Surface {
+                    val iksOks = remember { game }
+                    Text(
+                        text = "Game won: ${iksOks.gameWon}",
+                        modifier = Modifier.padding(16.dp)
+                    )
+                }
             }
         }
     }
