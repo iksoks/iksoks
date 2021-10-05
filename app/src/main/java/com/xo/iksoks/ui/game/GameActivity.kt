446a268cf234d97d094b3c167a76f79b57fc9f52
xo <revolutionaparis@gmail.com>
2021-10-05 19:39:33 +0200

    Inject GameViewModel in GameActivity

--- app/src/main/java/com/xo/iksoks/ui/game/GameActivity.kt
+++ app/src/main/java/com/xo/iksoks/ui/game/GameActivity.kt
@@ -3,6 +3,7 @@ package com.xo.iksoks.ui.game
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.viewModels
 import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
@@ -24,10 +25,14 @@ import com.xo.iksoks.Constants.BOARD_SIZE
 import com.xo.iksoks.domain.IksOks
 import com.xo.iksoks.ui.theme.IksOksTheme
 import com.xo.iksoks.domain.Square.*
+import com.xo.iksoks.ui.viewmodel.GameViewModel
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class GameActivity : ComponentActivity() {
+
+    val viewModel : GameViewModel by viewModels()
+
     @OptIn(ExperimentalFoundationApi::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
